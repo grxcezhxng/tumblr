4a811664d61eac330d23e494b693f4639148eca0
Grace Zhang <gracezhg@fb.com>
2021-06-24 10:01:08 -0700

    tumblr app

---
+++ Tumblr/photosTableViewCell.h
@@ -0,0 +1,18 @@
+//
+//  photosTableViewCell.h
+//  Tumblr
+//
+//  Created by gracezhg on 6/24/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface photosTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *photoView;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tumblr/photosTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  photosTableViewCell.m
+//  Tumblr
+//
+//  Created by gracezhg on 6/24/21.
+//
+
+#import "photosTableViewCell.h"
+
+@implementation photosTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
